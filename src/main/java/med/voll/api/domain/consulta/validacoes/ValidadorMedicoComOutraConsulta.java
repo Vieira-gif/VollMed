505d8dadae635ec5a38ae1af9beef98d221e3001
João Vieira <mutantinotrue@outlook.com>
2023-11-29 20:01:08 -0300

    Validador da disponibilidade do medico

---
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorMedicoComOutraConsulta.java
@@ -0,0 +1,16 @@
+package med.voll.api.domain.consulta.validacoes;
+
+import jakarta.validation.ValidationException;
+import med.voll.api.domain.consulta.ConsultaRepository;
+import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+
+public class ValidadorMedicoComOutraConsulta {
+
+    private ConsultaRepository repository;
+    public void validar(DadosAgendamentoConsulta dados){
+        var medicoPossuiOutraConsultaNoMesmoHorario = repository.existsByMedicoIdAndData(dados.idMedico(), dados.data());
+        if (medicoPossuiOutraConsultaNoMesmoHorario){
+            throw new ValidationException("Médico já possui outra consulta agendada nesse horario,");
+        }
+    }
+}
